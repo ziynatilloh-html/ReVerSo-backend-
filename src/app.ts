34ce152fc3d4acdac0db42907d37d3efce58f769
Ziynatilloh <ziynatillokht@gmail.com>
2025-02-25 17:00:08 +0900

    feat: express integration

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/** 2-SESSIONS **/
+
+/** 3-VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4-ROUTERS **/
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
-
 dotenv.config();
-
-console.log("PORT", process.env.PORT);
-console.log("MONGO_URL", process.env.MONGO_URL);
+import mongoose from "mongoose";
+import app from "./app";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3007;
+    app.listen(PORT, function () {
+      console.log(`The server running in ${PORT} port`);
+    });
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
