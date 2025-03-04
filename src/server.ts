e2e56ec4236849295b6ae5965070f0906bb14d8b
Ziynatilloh <ziynatillokht@gmail.com>
2025-03-04 18:19:38 +0900

    fix:  modify admin Login and Signup process

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3004;
+    const PORT = process.env.PORT ?? 3007;
     app.listen(PORT, function () {
       console.info(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);
--- src/service/Member.service.ts
+++ src/service/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { MemberType } from "../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/types/Error";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -14,6 +15,9 @@ class MemberService {
       .findOne({ memberType: MemberType.ADMIN })
       .exec();
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    console.log("after input:", input);
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = "";
@@ -26,12 +30,19 @@ class MemberService {
   public async processLogin(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne({
-        $or: [{ memberNick: 1 }, { memberPhone: 1 }, { email: 1 }],
+        $or: [
+          { memberNick: input.memberNick },
+          { memberPhone: input.memberPhone },
+          { memberEmail: input.memberEmail },
+        ],
       })
       .select("+memberPassword")
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_FOUND);
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
     if (!isMatch)
       throw new Errors(HttpCode.UNAUTHORIZED, Message.NO_MEMBER_FOUND);
     return await this.memberModel.findOne(member._id).exec();
